e2c00477b173dc487a06892b1d0db3f2910299b0
alessandro-gennaccari <alessandrogennaccari@gmail.com>
2023-02-15 11:48:23 +0100

    changed single column option style

--- src/GeneralTable/HeaderActionList/HeaderActionList.jsx
+++ src/GeneralTable/HeaderActionList/HeaderActionList.jsx
@@ -11,56 +11,60 @@ import { sortType } from "../tableAddons";
 import Style from "./HeaderActionList.module.css";
 
 function HeaderActionList({ column, tableRef, setHiddenColumns }) {
-  const [showActionList, setShowActionList] = useState(false);
+	const [showActionList, setShowActionList] = useState(false);
 
-  return (
-    <div onMouseLeave={() => setShowActionList(false)}>
-      <BiDotsVerticalRounded
-        onClick={() => setShowActionList(!showActionList)}
-        className={Style.headerActions}
-        size={"20"}
-      />
-      <ConditionalComponent condition={showActionList}>
-        <div className={Style.columnActions}>
-          <ConditionalComponent condition={column.sortable !== false}>
-            <div
-              onClick={() => {
-                tableRef.current.setSortField(column.field);
-                tableRef.current.setSortType(sortType.UP);
-              }}
-            >
-              Sort by ASC
-            </div>
-          </ConditionalComponent>
-          <ConditionalComponent condition={column.sortable !== false}>
-            <div
-              onClick={() => {
-                tableRef.current.setSortField(column.field);
-                tableRef.current.setSortType(sortType.DOWN);
-              }}
-            >
-              Sort by DESC
-            </div>
-          </ConditionalComponent>
+	return (
+		<div
+			onMouseLeave={() => setShowActionList(false)}
+		>
+			<BiDotsVerticalRounded
+				onClick={() => setShowActionList(!showActionList)}
+				className={Style.headerActions}
+				size={"20"}
+			/>
+			<ConditionalComponent condition={showActionList}>
+				<div className={Style.boxAction}>
+					<div className={Style.columnActions}>
+						<ConditionalComponent condition={column.sortable !== false}>
+							<div
+								onClick={() => {
+									tableRef.current.setSortField(column.field);
+									tableRef.current.setSortType(sortType.UP);
+								}}
+							>
+								Sort by ASC
+							</div>
+						</ConditionalComponent>
+						<ConditionalComponent condition={column.sortable !== false}>
+							<div
+								onClick={() => {
+									tableRef.current.setSortField(column.field);
+									tableRef.current.setSortType(sortType.DOWN);
+								}}
+							>
+								Sort by DESC
+							</div>
+						</ConditionalComponent>
 
-          <div
-            onClick={() => {
-              setHiddenColumns((oldState) => [...oldState, column.field]);
-            }}
-          >
-            Hide this columns
-          </div>
-          <div
-            onClick={() => {
-              setHiddenColumns([]);
-            }}
-          >
-            Show all columns
-          </div>
-        </div>
-      </ConditionalComponent>
-    </div>
-  );
+						<div
+							onClick={() => {
+								setHiddenColumns((oldState) => [...oldState, column.field]);
+							}}
+						>
+							Hide this columns
+						</div>
+						<div
+							onClick={() => {
+								setHiddenColumns([]);
+							}}
+						>
+							Show all columns
+						</div>
+					</div>
+				</div>
+			</ConditionalComponent>
+		</div>
+	);
 }
 
 export default HeaderActionList;
